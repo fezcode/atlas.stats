910f082de2959d4e249ed5ea8b8bc9c32b27d570
fezcode <samil.bulbul@gmail.com>
2026-02-20 23:34:59 +0300

    feat: add version flag support

--- main.go
+++ main.go
@@ -7,7 +7,14 @@ import (
 	"atlas.stats/pkg/ui"
 )
 
+var Version = "dev"
+
 func main() {
+	if len(os.Args) > 1 && (os.Args[1] == "-v" || os.Args[1] == "--version") {
+		fmt.Printf("atlas.stats v%s\n", Version)
+		return
+	}
+
 	if err := ui.Start(); err != nil {
 		fmt.Printf("Error starting UI: %v\n", err)
 		os.Exit(1)
